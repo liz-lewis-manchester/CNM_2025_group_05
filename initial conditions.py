c2fe62128f6488444e091a96530ff4ed99b0af8f
folajimizakari <oluwafolajimi.zakari@student.manchester.ac.uk>
2025-12-11 12:42:27 +0000

    Update initial conditions.py
    rechanged the function name of the initial conditions

--- initial conditions.py
+++ initial conditions.py
@@ -3,8 +3,8 @@ import numpy as np
 from scipy.interpolate import interp1d
 
 # Load initial condition data from a CSV file.
-def load_initial_conditions("initial_conditions.csv"):
-    df = pd.read_csv("initial_conditions.csv")
+def load_initial_conditions(csv_paths):
+    df = pd.read_csv(csv_paths)
     # Ensure correct column names
     required_cols = ["Distance (m)", "Concentartion (µg/m_ )"]
     if not all(col in df.columns for col in required_cols):
@@ -12,9 +12,9 @@ def load_initial_conditions("initial_conditions.csv"):
     return df["Distance (m)"].values, df["Concentraion (µg/m_ )"].values
 
 # Now we interpolate concentration values onto the model grid
-def interpolate_initial_conditions("initial_conditions.csv", grid):
+def interpolate_initial_conditions(csv_paths, grid):
     # Load raw CSV data 
-    raw_Dist, raw_Conc = load_initial_conditions("initial_conditions.csv")
+    raw_Dist, raw_Conc = load_initial_conditions(csv_paths)
     # Create linear interpolation function
     interpolator = interpld(raw_Dist, raw_Conc, kind="linear", fill_value="extrapolate")    # the last bit of code allows values outside the CSV range
     # Now we evaluate on model grid
